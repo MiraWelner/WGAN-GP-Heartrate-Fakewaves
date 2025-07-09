ffc34b78332fba94326a758eaaf0326d76244f67
Mira Welner <miraewelner@gmail.com>
2025-07-09 15:27:52 -0400

    improved documentation

--- train_gan_heartrate.py
+++ train_gan_heartrate.py
@@ -1,11 +1,10 @@
 """
 Mira Welner
 July 2025
-This script generates a set of square and triangle waves, then trains the WGAN-GP on them. It varies the
-wavelength, waveheight, whether it is evenly spaces, randomly spaced, or pattern spaced, and the wave peak length.
-
-It then saves the figures and models.
+This script generates a loads the qt and rr intervals from the proccessed_data folder
+and plots the WGAN-GP's output when trained on the intervals
 """
+
 import numpy as np
 import torch
 import sys
@@ -54,17 +53,17 @@ def plot_mean_and_diff(rr_test_squeeze, rr_gen_output, qt_test_squeeze, qt_gen_o
     #axes[0].fill_between(range(rr_gen_output.shape[1]), lower_bound, upper_bound, alpha=0.5, label="Mean Difference Between Real and Gen")
     axes[0].plot(mean_rr_real, label="Mean Real Data", color='red')
     axes[0].plot(mean_rr_gen, label="Mean Generated Data", color='orange')
-
     axes[0].legend()
-    axes[0].set_xlabel("Time (s)")
-    axes[0].set_xticks(np.arange(0, 3501, 500), [str(i) for i in range(8)])
+    axes[0].set_xlabel("Time (m)")
+    axes[0].set_ylabel("Scaled RR Invervals")
+    axes[0].set_xticks(np.arange(0, 3501, 600), [str(i) for i in range(6)])
     rr_gen_mean = np.mean(rr_gen_output)
     rr_gen_median = np.median(rr_gen_output)
     rr_gen_std = np.std(rr_gen_output)
     rr_real_mean = np.mean(rr_test_squeeze)
     rr_real_median = np.median(rr_test_squeeze)
     rr_real_std = np.std(rr_test_squeeze)
-    axes[0].set_title(f"Comparison Between original RR intervals and WGAN-Generated RR intervals \n Generated Mean: {rr_gen_mean:.3f}, Generated Median {rr_gen_median:.3f}, Generated STD {rr_gen_std:.3f}, Original Mean: {rr_real_mean:.3f}, Original Median {rr_real_median:.3f}, Original STD {rr_real_std:.3f}")
+    axes[0].set_title(f"Heartrate Over Time (RR Invervals) \n Generated Mean: {rr_gen_mean:.3f}, Generated Median {rr_gen_median:.3f}, Generated STD {rr_gen_std:.3f}, Original Mean: {rr_real_mean:.3f}, Original Median {rr_real_median:.3f}, Original STD {rr_real_std:.3f}")
 
     #plot triangle wave data on lower plot
     mean_qt_real = np.mean(qt_test_squeeze, axis=0)
@@ -74,15 +73,16 @@ def plot_mean_and_diff(rr_test_squeeze, rr_gen_output, qt_test_squeeze, qt_gen_o
     axes[1].plot(mean_qt_real, label="Mean Real Data", color='red')
     axes[1].plot(mean_qt_gen, label="Mean Generated Data", color='orange')
     axes[1].legend()
-    axes[1].set_xlabel("Time (s)")
-    axes[1].set_xticks(np.arange(0, 3501, 500), [str(i) for i in range(8)])
+    axes[1].set_xlabel("Time (m)")
+    axes[1].set_ylabel("Scaled QT Intervals")
+    axes[1].set_xticks(np.arange(0, 3501, 600), [str(i) for i in range(6)])
     qt_gen_mean = np.mean(qt_gen_output)
     qt_gen_median = np.median(qt_gen_output)
     qt_gen_std = np.std(qt_gen_output)
     qt_real_mean = np.mean(qt_test_squeeze)
     qt_real_median = np.median(qt_test_squeeze)
     qt_real_std = np.std(qt_test_squeeze)
-    axes[1].set_title(f"Comparison Between original QT intervals and WGAN-Generated QT intervals \n Generated Mean: {qt_gen_mean:.3f}, Generated Median {qt_gen_median:.3f}, Generated STD {qt_gen_std:.3f}, Original Mean: {qt_real_mean:.3f}, Original Median {qt_real_median:.3f}, Original STD {qt_real_std:.3f}")
+    axes[1].set_title(f"Heartrate Over Time (QT Invervals) \n Generated Mean: {qt_gen_mean:.3f}, Generated Median {qt_gen_median:.3f}, Generated STD {qt_gen_std:.3f}, Original Mean: {qt_real_mean:.3f}, Original Median {qt_real_median:.3f}, Original STD {qt_real_std:.3f}")
 
     plt.tight_layout()
     plt.savefig("figures/wgan_comparison/qt_rr.png")
@@ -92,15 +92,17 @@ def plot_mean_and_diff(rr_test_squeeze, rr_gen_output, qt_test_squeeze, qt_gen_o
 def plot_examples(rr_gen, qt_gen, show=False):
     _, axes = plt.subplots(2,1, figsize=(17,7))
     axes[0].plot(rr_gen[0], color='cornflowerblue')
-    axes[0].set_title("Example of Generated RR Output")
-    axes[0].set_xticks(np.arange(0, 3501, 500), [str(i) for i in range(8)])
-    axes[0].set_xlabel("Time (s)")
+    axes[0].set_title("Example of Generated Heartrate")
+    axes[0].set_xticks(np.arange(0, 3501, 600), [str(i) for i in range(6)])
+    axes[0].set_xlabel("Time (m)")
+    axes[0].set_ylabel("Scaled RR Interval")
 
 
     axes[1].plot(qt_gen[0], color='cornflowerblue')
-    axes[1].set_title("Example of Generated QT Wave")
-    axes[1].set_xticks(np.arange(0, 3501, 500), [str(i) for i in range(8)])
-    axes[1].set_xlabel("Time (s)")
+    axes[1].set_title("Example of Generated Heartrate")
+    axes[1].set_xticks(np.arange(0, 3501, 600), [str(i) for i in range(6)])
+    axes[1].set_xlabel("Time (m)")
+    axes[1].set_ylabel("Scaled QT Interval")
     plt.tight_layout()
     plt.savefig("figures/samples/qt_rr.png")
     if show:
@@ -119,7 +121,7 @@ rr = pd.read_csv('processed_data/rr_processed.csv', index_col=0)
 
 
 train_rr, test_rr, train_qt, test_qt = make_train_test_dataloaders(rr, qt)
-gen_rr, gen_qt = train_store_gan(train_rr, train_qt)
+gen_rr, gen_qt = train_store_gan(train_rr, train_qt, just_load=True)
 
 rr_gen_output = np.array([gen_rr(torch.randn(1, 100).cuda()).cpu().detach().numpy().squeeze() for _ in range(500)])
 rr_test_squeeze = test_rr.squeeze()
