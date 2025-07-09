8967dd6f880dd19b390a799e79c359681d05334c
Mira Welner <miraewelner@gmail.com>
2025-07-09 14:51:48 -0400

    fixed QT bug - finalized QT plotting

--- process_heartrate_data.py
+++ process_heartrate_data.py
@@ -2,25 +2,23 @@
 Mira Welner
 June 2025
 
-This script loads Dr. Dey's heartrate data and proccesses it to be 1/10th of a second, with the y value being the ms in the r-r
-distance at that individual decasecond.
+This script loads Dr. Cong's PPG and ECG data, removes outliers, and interpolates the PPG data
+which is 125 hz to be 500hz like the ECG data.
+
+As of now, this algorithm does NOT remove noise because my previous notch filter method
+cannot easily be automated. So, my hope is that the model will remove the noise.
 """
 
 import pandas as pd
-from glob import glob
-import matplotlib.pyplot as plt
-import sys
 import numpy as np
 from scipy.interpolate import interp1d
-from random import randrange
+from glob import glob
 
-frac_sec = 2 #the x values are half of a second
-rr_df = pd.DataFrame()
-qt_df = pd.DataFrame()
+frac_sec = 10 #the x values are 100th of a second
 
 def process_rr(rr_distance_ms, snip_len = 3500):
     bpm = 60000/rr_distance_ms
-    scaled_heartrate = bpm/75-1
+    scaled_heartrate = bpm/75 -1
     num_samples = len(scaled_heartrate)//snip_len
     scaled_heartrate_trimmed = scaled_heartrate[:num_samples*snip_len]
     heartrate_snips = scaled_heartrate_trimmed.reshape(num_samples, snip_len)
@@ -28,69 +26,73 @@ def process_rr(rr_distance_ms, snip_len = 3500):
     return snip_df
 
 def process_qt(qt_distance_ms, snip_len = 3500):
-    num_samples = len(qt_distance_ms)//snip_len
-    scaled_heartrate_trimmed = qt_distance_ms[:num_samples*snip_len]
+    scaled_qt = qt_distance_ms/350-1
+    num_samples = len(scaled_qt)//snip_len
+    scaled_heartrate_trimmed = scaled_qt[:num_samples*snip_len]
     heartrate_snips = scaled_heartrate_trimmed.reshape(num_samples, snip_len)
     snip_df = pd.DataFrame(heartrate_snips)
     return snip_df
 
-
-def proccess_datafile(path):
-    """
-    This only works on the 18-58-25 files, and the
-    07-15-37 files, or files of similar format
-    """
+def proccess_file(path):
     file = pd.read_csv(path)
     rr_distance_ms = file.iloc[:, 3]
-    qt_distance_ms = np.array([0.0 if val == '  ' else val for val in file.iloc[:, 4]], dtype=float)
+    qt_distance_ms = pd.Series(pd.to_numeric(file.iloc[:, 4], errors='coerce')).fillna(0)
     timestamps = file.iloc[:,1]
-
-    x = np.arange(min(timestamps), max(timestamps)-1, 1/frac_sec)
-
+    qt_times_s = [sum(rr_distance_ms[:i])/1000 for i in range(len(rr_distance_ms))]
     f_interp_rr = interp1d(timestamps, rr_distance_ms, kind='linear')
-    f_interp_qt = interp1d(timestamps, qt_distance_ms, kind='linear')
-    fig = plt.figure()
-    plt.plot(f_interp_rr(x))
-    plt.show()
-    return process_rr(f_interp_rr(x)), process_qt(f_interp_qt(x))
+    x_rr = np.arange(min(timestamps), max(timestamps)-1, 1/frac_sec)
+    interpolated_rr = f_interp_rr(x_rr)
 
+    f_interp_qt = interp1d(qt_times_s, qt_distance_ms, kind='linear')
+    x_qt = np.arange(min(qt_times_s), max(qt_times_s)-1, 1/frac_sec)
+    interpolated_qt = f_interp_qt(x_qt)
+    return process_rr(interpolated_rr), process_qt(interpolated_qt)
+
+
+
+rr = pd.DataFrame()
+qt = pd.DataFrame()
 
 for i in range(1,5):
-    rr_data, qt_data = proccess_datafile(f"heartrate_data/18-58-25_8_hours_part{i}_v2_wholecaseRRiQTi.csv")
-    rr_df = pd.concat([rr_df, rr_data])
-    qt_df = pd.concat([qt_df, qt_data])
-
+    path = f"heartrate_data/18-58-25_8_hours_part{i}_v2_wholecaseRRiQTi.csv"
+    new_rr, new_qt = proccess_file(path)
+    rr = pd.concat([rr, new_rr])
+    qt = pd.concat([qt, new_qt])
 
 for i in range(1,5):
-    rr_data, qt_data = proccess_datafile(f"heartrate_data/07-15-37_8_hours_part{i}_v2_wholecaseRRiQTi.csv")
-    rr_df = pd.concat([rr_df, rr_data])
-    qt_df = pd.concat([qt_df, qt_data])
+    path = f"heartrate_data/07-15-37_8_hours_part{i}_v2_wholecaseRRiQTi.csv"
+    new_rr, new_qt = proccess_file(path)
+    rr = pd.concat([rr, new_rr])
+    qt = pd.concat([qt, new_qt])
 
 
-# the everyRRQTinputIntoEntropy files are proccessed differently because they are formatted
-# differently for some reason
-"""
-everyRRQT_time_data = []
-everyRRQT_rr_data = []
-everyRRQT_qt_data = []
+#the below files are very short so they need to be concatinated before being processed
+
+short_files_rr = []
+short_files_qt = []
+short_files_timestamps = []
 for i in range(1,28):
     path = glob(f"heartrate_data/11-03-38_8_hours_part2_v2_everyRRQTinputIntoEntropy_Rel{i}_Abs*")[0]
     file = pd.read_csv(path)
-    x = file.iloc[:,0]
-    if len(everyRRQT_time_data):
-        x+=everyRRQT_time_data[-1]
+    timestamps = file.iloc[:,0]
+    if len(short_files_timestamps):
+        timestamps+=short_files_timestamps[-1]
+    rr_distance_s = np.asarray(file.iloc[:,1])
+    qt_distance_s = np.asarray(file.iloc[:,2])
+    short_files_rr.extend(rr_distance_s*1000)
+    short_files_qt.extend(qt_distance_s*1000)
+    short_files_timestamps.extend(timestamps)
 
-    everyRRQT_rr_data.extend(np.asarray(file.iloc[:,1])*1000)
-    everyRRQT_qt_data.extend(np.asarray(file.iloc[:,2])*1000)
-    everyRRQT_time_data.extend(x)
+qt_times_s = [sum(short_files_rr[:i])/1000 for i in range(len(short_files_rr))]
+f_interp_rr = interp1d(short_files_timestamps, short_files_rr, kind='linear')
+x_rr = np.arange(min(short_files_timestamps), max(short_files_timestamps)-1, 1/frac_sec)
+interpolated_rr = f_interp_rr(x_rr)
 
-f_interp_rr = interp1d(everyRRQT_time_data, everyRRQT_rr_data, kind='linear')
-f_interp_qt = interp1d(everyRRQT_time_data, everyRRQT_qt_data, kind='linear')
-x_ms = np.arange(min(everyRRQT_time_data), max(everyRRQT_time_data)-1, 1/frac_sec)
-rr_df = pd.concat([rr_df, process_rr(f_interp_rr(x_ms))])
-qt_df = pd.concat([qt_df, process_rr(f_interp_qt(x_ms))])
-"""
+f_interp_qt = interp1d(qt_times_s, short_files_qt, kind='linear')
+x_qt = np.arange(min(qt_times_s), max(qt_times_s)-1, 1/frac_sec)
+interpolated_qt = f_interp_qt(x_qt)
+rr = pd.concat([rr, process_rr(interpolated_rr)])
+qt = pd.concat([qt, process_qt(interpolated_qt)])
 
-
-rr_df.to_csv('processed_data/rr_processed.csv')
-qt_df.to_csv('processed_data/qt_processed.csv')
+rr.to_csv('processed_data/rr_processed.csv')
+qt.to_csv('processed_data/qt_processed.csv')
