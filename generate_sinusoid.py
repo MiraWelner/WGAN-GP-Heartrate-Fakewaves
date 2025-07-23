6c14e340b54102f48ca83f3a1792acae26c69fc0
Mira Welner <miraewelner@gmail.com>
2025-07-23 10:44:01 -0400

    improved plots further

--- generate_sinusoid.py
+++ generate_sinusoid.py
@@ -12,16 +12,23 @@ import matplotlib.pyplot as plt
 from scipy.signal import find_peaks
 plt.rcParams.update({'font.size': 7})
 
-wave_0 = [np.sin(i/(1.5*np.pi)) for i in range(600)]
-wave_1 = [np.sin(i/(8*np.pi))*0.7 + np.cos(i)*0.3 for i in range(600)]
-wave_2 = [np.sin(i/(16*np.pi))*0.8 + np.cos(i/8)*0.2 for i in range(600)]
-wave_3 = [np.sin(i/(32*np.pi)) for i in range(600)]
-wave_4 = [np.sin(i/(32*np.pi))*0.6+np.cos(i/(32*np.pi))*0.4 for i in range(600)]
-wave_5 = [np.sin(i/(4*np.pi))*0.4 + np.sin(i/(2*np.pi+2))*0.6 for i in range(600) ]
-wave_6 = [np.sin(i/(1.5*np.pi))/2 - np.cos(i*100)/2 for i in range(600)]
-wave_7 = [np.sin(i/(4*np.pi)-0.5*np.pi)/3 + np.sin(i/(2*np.pi+2))/3+ np.sin(i/(9*np.pi+2))/3 for i in range(600)]
-wave_8 = [np.sin(i/(4*np.pi))/3 +np.sin(i/(16*np.pi+2))/3-np.sin(i/(0.2*np.pi+2))/3 for i in range(600)]
-wave_9 = [np.cos(i/(40*np.pi))/2-np.sin(i/(0.2*np.pi+2))/2 for i in range(600)]
+period = np.linspace(0, 4 * np.pi, 600)
+
+wave_0 = [np.sin(10*i) for i in period]
+wave_1 = [np.sin(i/2)*0.7 + np.sin(i)*0.2 -np.sin(i*20)*0.1 for i in period]
+wave_2 = [np.sin(i)*0.8+np.sin(i*5)*0.2 for i in period]
+wave_3 = [np.sin(i/2) for i in period]
+wave_4 = [np.sin(6*i)*0.6 + np.sin(5*i)*0.4 for i in period]
+wave_5 = [np.sin(i*5)*0.50 + np.sin(i*8)*0.25 + np.sin(i)*0.25 for i in period]
+wave_6 = [np.sin(i)*0.5-np.sin(20* i)*0.5 for i in period]
+wave_7 = [np.sin(i*5)/4 + np.sin(i*3)/4+ np.sin(i*4)/4 - np.sin(i*40)/4 for i in period]
+wave_8 = [np.sin(i*2)*0.3 +np.sin(i+np.pi)*0.4-np.sin(i)*0.3 for i in period]
+wave_9 = [np.sin(i*2)*0.1 +
+          np.sin(i*3)*0.2 +
+          np.sin(i*6)*0.3 +
+          np.sin(i*8)*0.3 -
+          np.sin(i*40)*0.1
+          for i in period]
 
 sin_heartrate_sim = np.array([wave_0, wave_1, wave_2, wave_3, wave_4, wave_5, wave_6, wave_7, wave_8,wave_9])
 fig, axes = plt.subplots(5,4,figsize=(17,9))
@@ -29,7 +36,7 @@ for itr, wave in enumerate(sin_heartrate_sim):
     axes[itr//2,itr%2*2].plot(wave)
     axes[itr//2,itr%2*2].set_ylim(-1.1,1.1)
     axes[itr//2,itr%2*2].set_yticks([-1,0,1])
-    axes[itr//2,itr%2*2].set_title(f'mean:{np.mean(wave):.2f}, std:{np.std(wave):.2f}')
+    axes[itr//2,itr%2*2].set_title(f'mean:{np.mean(wave):.3f}, std:{np.std(wave):.2f}')
     axes[itr//2,itr%2*2].set_ylabel(f"Sinusoid {itr}")
     # Compute FFT and get magnitude spectrum
     n_half = len(wave) // 2
@@ -41,10 +48,10 @@ for itr, wave in enumerate(sin_heartrate_sim):
     posmag =  magnitude[:n_half-2]
     # Plot only positive frequencies (first half of spectrum)
     axes[itr//2,itr%2*2+1].plot(posfreqs, posmag)
-    peaks = posfreqs[find_peaks(posmag)[0]]
+    peaks = posfreqs[find_peaks(posmag, height=10)[0]]
     peaks_list = ', '.join(f"{num:.3f}" for num in peaks)
     axes[itr//2,itr%2*2+1].set_title(f'Peak frequencies: {peaks_list}')
-    axes[itr//2,itr%2*2+1].set_xlim(0,0.2)
+   # axes[itr//2,itr%2*2+1].set_xlim(0,0.2)
 fig.tight_layout()
 np.savetxt("processed_data/sinusoid.csv", sin_heartrate_sim, delimiter=",")
 plt.savefig('figures/sinusoid.png')
