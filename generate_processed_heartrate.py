fd58c1563bd8612a6b9b5cfcc868b4246e31e267
Mira Welner <miraewelner@gmail.com>
2025-07-29 17:00:44 -0400

    fixed data processing with time series

--- generate_processed_heartrate.py
+++ generate_processed_heartrate.py
@@ -9,7 +9,6 @@ It then creates a training table of length=6000 for each snip for both RR and QT
 
 import numpy as np
 from scipy.interpolate import interp1d
-from glob import glob
 import matplotlib.pyplot as plt
 
 heartrate_hz = 10 #the x values are 10th of a second
@@ -20,69 +19,36 @@ def scale_data(x:np.ndarray) -> np.ndarray:
     x_scaled = 2*(x - x.min()) / (x.max() - x.min())-1
     return x_scaled
 
-def process_qt(qt_distance_ms):
-    """
-    The QT x value is represented by the sum of the previous R-R distances
-    rather than the timestamp.
-    """
-    scaled_qt = qt_distance_ms/350-1
-    num_samples = len(scaled_qt)//snip_len
-    scaled_heartrate_trimmed = scaled_qt[:num_samples*snip_len]
-    heartrate_snips = scaled_heartrate_trimmed.reshape(num_samples, snip_len)
-    return heartrate_snips
-
-def proccess_heartrate_file(rr_distance_ms,timestamps):
+def process_heartrate_file(rr_distance_ms):
+    rr_distance_ms = np.clip(rr_distance_ms, a_min=None, a_max=2000)
+    x_axis = np.cumsum(rr_distance_ms)
     bpm = 60000/rr_distance_ms
-    f_interp_bpm = interp1d(timestamps, bpm, kind='linear')
-    x_bpm = np.arange(timestamps.min(), timestamps.max()-1, 1/heartrate_hz)
+    f_interp_bpm = interp1d(x_axis, bpm, kind='linear')
+    x_bpm = np.arange(x_axis[0], x_axis[-1]-1, 1000/heartrate_hz)
     interpolated_bpm = f_interp_bpm(x_bpm)
-    return interpolated_bpm
+    return interpolated_bpm, x_bpm
 
-rr_18 = []
-qt_18 = []
-for i in range(1,5):
-    file_path = f"heartrate_data/18-58-25_8_hours_part{i}_v2_wholecaseRRiQTi.csv"
-    data_file = np.genfromtxt(file_path, skip_header=1, usecols=(1, 3), delimiter=',', filling_values=0.0, dtype=float)
-    timestamps, rr_distance_ms = data_file.T
-    new_rr = proccess_heartrate_file(rr_distance_ms,timestamps)
-    rr_18.extend(new_rr)
+def process_patient_data(name:str, parts:int):
+    bpm_data = []
+    all_x_axis = np.array([])
+    for i in range(1,parts+1):
+        file_path = f"heartrate_data/{name}_24_hours_part{i}_v3_wholecaseRRiQTi.csv"
+        data_file = np.genfromtxt(file_path, skip_header=1, usecols=([3]), delimiter=',', filling_values=0.0, dtype=float)
+        rr_distance_ms = data_file.T
+        new_bpm, x_axis = process_heartrate_file(rr_distance_ms)
+        bpm_data.extend(new_bpm)
+        if len(all_x_axis):
+            prev_max = all_x_axis[-1]
+            all_x_axis = np.append(all_x_axis, x_axis+prev_max)
+        else:
+            all_x_axis = np.append(all_x_axis, x_axis)
+    data = np.vstack([np.array(bpm_data), all_x_axis])
+    np.savetxt(f"processed_data/heartrate_{name}.csv", data, delimiter = ",")
+    return data
 
-rr_07 = []
-qt_07 = []
-for i in range(1,5):
-    file_path = f"heartrate_data/07-15-37_8_hours_part{i}_v2_wholecaseRRiQTi.csv"
-    data_file = np.genfromtxt(file_path, skip_header=1, usecols=(1, 3), delimiter=',', filling_values=0.0, dtype=float)
-    timestamps, rr_distance_ms = data_file.T
-    new_rr = proccess_heartrate_file(rr_distance_ms,timestamps)
-    rr_07.extend(new_rr)
-
-
-#the below files are very short so they need to be concatinated before being processed
-short_files_rr = []
-short_files_qt = []
-short_files_timestamps = []
-for i in range(1,28):
-    file_path = glob(f"heartrate_data/11-03-38_8_hours_part2_v2_everyRRQTinputIntoEntropy_Rel{i}_Abs*")[0]
-    data_file = np.genfromtxt(file_path, skip_header=1, delimiter=',', filling_values=0.0, dtype=float)
-    timestamps = data_file[:,0]
-    if len(short_files_timestamps):
-        timestamps+=short_files_timestamps[-1]
-    rr_distance_s = data_file[:, 1].T
-    short_files_rr.extend(rr_distance_s*1000)
-    short_files_timestamps.extend(timestamps)
-
-rr_11 = proccess_heartrate_file(np.array(short_files_rr),np.array(short_files_timestamps))
-
-fig = plt.figure()
-plt.plot(rr_11.flatten())
-plt.savefig('heartrate_11.png')
-plt.show()
-plt.plot(rr_07)
-plt.savefig('heartrate_07.png')
-plt.show()
-plt.plot(rr_18)
-plt.savefig('heartrate_18.png')
-plt.show()
-np.savetxt("processed_data/heartrate_18.csv", np.vstack(rr_18), delimiter = ",")
-np.savetxt("processed_data/heartrate_07.csv", np.vstack(rr_07), delimiter = ",")
-np.savetxt("processed_data/heartrate_11.csv", rr_11, delimiter = ",")
+patient_06 = process_patient_data('06-31-24', parts=2)
+patient_09 = process_patient_data('09-40-14', parts=5)
+patient_10 = process_patient_data('10-48-45', parts=4)
+patient_11 = process_patient_data('11-03-38', parts=5)
+patient_13 = process_patient_data('13-22-23', parts=4)
+patient_14 = process_patient_data('14-17-50', parts=4)
