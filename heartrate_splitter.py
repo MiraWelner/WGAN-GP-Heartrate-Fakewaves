dc827137e226ff9dfee64fa60c8e51ea4586e085
Mira Welner <miraewelner@gmail.com>
2025-08-15 17:03:43 -0400

    added kurtosis and skew results

--- heartrate_splitter.py
+++ heartrate_splitter.py
@@ -5,11 +5,12 @@ from wgan import train_wgan, Generator
 from scipy.signal import find_peaks
 from matplotlib.lines import Line2D
 from torch.utils.data import TensorDataset, DataLoader
+from scipy.stats import kurtosis, skew
 
 
 patient_names = '06-31-24', '09-40-14', '10-48-45', '11-03-38', '13-22-23', '14-17-50'
 snip_len=60*60
-batch_size = 16
+batch_size = 32
 
 
 def make_GAN_test_set(dataset:np.ndarray):
@@ -44,13 +45,13 @@ def split_data(patient):
     second_half = shaped_data[int(snip_len/2):,:]
     return first_half, second_half, heartrate_min, heartrate_max
 
-def train_store_gan(train, name, just_load, epochs):
+def train_store_gan(train, name, patient, just_load, epochs):
     """
     if just_load, then we just load the generator and return it.
     If just_load is false, then train the new GAN and store it with
     the same naming conventions.
     """
-    gan_name = f"models/generator_heartrate_{name}.pth"
+    gan_name = f"models/generator_heartrate_{name}_{patient}.pth"
     if not just_load:
         gen, _ = train_wgan(train, epochs=epochs, signal_length=int(snip_len//2))
         torch.save(gen.state_dict(), gan_name)
@@ -68,69 +69,84 @@ def get_synthetic_outputs(gan, iterations, latent_dim=100):
     return synthetic_output
 
 def final_plot(synth_data:list, ground_truth_data:list, data_max:float, data_min:float, name:str, alpha=0.1):
-    fig, axes = plt.subplots(len(synth_data),3,figsize=(18,8),constrained_layout=True)
-    for itr, (synthetic, ground_truth) in enumerate(zip(synth_data, ground_truth_data)):
-        synthetic =   ((synthetic + 1) / 2) * (data_max - data_min) + data_min
-        ground_truth =  ((ground_truth + 1) / 2) * (data_max - data_min) + data_min
+    fig, axes = plt.subplots(len(synth_data)**2,3,figsize=(18,8),constrained_layout=True)
+    for i, ground_truth in enumerate(ground_truth_data):
+        for j, synthetic in enumerate(synth_data):
 
-        wave_ax = axes[itr,0]
-        fft_ax = axes[itr,1]
-        fft_ax.set_ylim(0,10)
-        wave_ax.set_ylim(0.5,1.2)
-        text_ax = axes[itr,2]
+            itr = 2*i+j
+            scaled_synth =   ((synthetic + 1) / 2) * (data_max - data_min) + data_min
+            scaled_gt =  ((ground_truth + 1) / 2) * (data_max - data_min) + data_min
 
-        for it, synth_wave in enumerate(synthetic):
-            wave_ax.plot(np.cumsum(synth_wave), synth_wave, alpha=alpha, color='red')
-            synth_freq, synth_mag, synth_peaks = fft_transform(synth_wave)
-            fft_ax.plot(synth_freq[1:], synth_mag[1:], alpha=alpha, color='red')
+            wave_ax = axes[itr,0]
+            fft_ax = axes[itr,1]
+            fft_ax.set_ylim(0,10)
+            wave_ax.set_ylim(0.5,1.2)
+            text_ax = axes[itr,2]
 
-        for it, gt_wave in enumerate(ground_truth):
-            gt_squeezed = gt_wave.squeeze()
-            wave_ax.plot(np.cumsum(gt_squeezed), gt_squeezed, alpha=alpha, color='blue')
-            freq, mag, _ = fft_transform(gt_squeezed)
-            fft_ax.plot(freq[1:], mag[1:], alpha=alpha, color='blue')
+            for synth_wave in scaled_synth:
+                wave_ax.plot(np.cumsum(synth_wave), synth_wave, alpha=alpha, color='red')
+                synth_freq, synth_mag, synth_peaks = fft_transform(synth_wave)
+                fft_ax.plot(synth_freq[1:], synth_mag[1:], alpha=alpha, color='red')
 
-        synth_mean = f'{np.mean(synthetic):.3f} \u00B1 {np.std(np.mean(synthetic, axis=1)):.2f}'
-        synth_std = f'{np.mean(np.std(synthetic, axis=1)):.3f} \u00B1 {np.std(np.std(synthetic, axis=1)):.2f}'
+            for gt_wave in scaled_gt:
+                gt_squeezed = gt_wave.squeeze()
+                wave_ax.plot(np.cumsum(gt_squeezed), gt_squeezed, alpha=alpha, color='blue')
+                freq, mag, _ = fft_transform(gt_squeezed)
+                fft_ax.plot(freq[1:], mag[1:], alpha=alpha, color='blue')
 
-        gt_mean = f'{np.mean(ground_truth):.3f} \u00B1 {np.std(np.mean(ground_truth, axis=2)):.2f}'
-        gt_std = f'{np.mean(np.std(ground_truth, axis=2)):.3f} \u00B1 {np.std(np.std(ground_truth, axis=2)):.2f}'
-
-        wave_ax.set_ylabel("R-R Interval (s)")
-        wave_ax.set_title(f"Scaled R-R Distances, part {itr}")
-
-        legend_lines = [Line2D([0], [0], color='blue'),  Line2D([0], [0], color='red')]
-        wave_ax.legend(legend_lines, [f"Ground Truth: {len(ground_truth)} samples", f"Synthetic: {len(synthetic)} samples"])
-
-        text_ax.text(0.01, 0.95, f'Ground Truth Mean:{gt_mean}', ha='left', va='top')
-        text_ax.text(0.01, 0.80, f'Ground Truth STD:{gt_std}', ha='left', va='top')
-        text_ax.text(0.01, 0.65,f'Synthetic Data Mean: {synth_mean}', ha='left', va='top')
-        text_ax.text(0.01, 0.50, f'Synthetic Data STD: {synth_std}', ha='left', va='top')
-
-        text_ax.set_xticks([])
-        text_ax.set_yticks([])
+            synth_mean = f'{np.mean(scaled_synth):.3f} \u00B1 {np.std(np.mean(scaled_synth, axis=1)):.2f}'
+            synth_std = f'{np.mean(np.std(scaled_synth, axis=1)):.3f} \u00B1 {np.std(np.std(scaled_synth, axis=1)):.2f}'
+            synth_kurtosis = f'{np.mean(kurtosis(scaled_synth, axis=1)):.3f} \u00B1 {np.std(kurtosis(scaled_synth, axis=1)):.2f}'
+            synth_skew = f'{np.mean(skew(scaled_synth, axis=1)):.3f} \u00B1 {np.std(skew(scaled_synth, axis=1)):.2f}'
 
 
-        if itr==0:
-            fft_ax.set_title("Heartrate FFT")
-            wave_ax.set_xlabel("Time (s)")
+            gt_mean = f'{np.mean(scaled_gt):.3f} \u00B1 {np.std(np.mean(scaled_gt, axis=2)):.2f}'
+            gt_std = f'{np.mean(np.std(scaled_gt, axis=2)):.3f} \u00B1 {np.std(np.std(scaled_gt, axis=2)):.2f}'
+            gt_kurtosis = f'{np.mean(kurtosis(scaled_gt, axis=2)):.3f} \u00B1 {np.std(kurtosis(scaled_gt, axis=2)):.2f}'
+            gt_skew = f'{np.mean(skew(scaled_gt, axis=2)):.3f} \u00B1 {np.std(skew(scaled_gt, axis=2)):.2f}'
 
-        else:
-            wave_ax.set_xticks([])
-            fft_ax.set_xticks([])
+            legend_lines = [Line2D([0], [0], color='blue'),  Line2D([0], [0], color='red')]
+            wave_ax.legend(legend_lines, [f"Ground Truth Part {i+1}: {len(scaled_gt)} samples", f"Synthetic From GAN Trained on Part {j+1}: {len(scaled_synth)} samples"])
+
+            text_ax.text(0.01, 0.95, f'Ground Truth Mean:{gt_mean}', ha='left', va='top')
+            text_ax.text(0.51, 0.95, f'Synthetic Data Mean: {synth_mean}', ha='left', va='top')
+
+            text_ax.text(0.01, 0.80, f'Ground Truth STD:{gt_std}', ha='left', va='top')
+            text_ax.text(0.51, 0.80, f'Synthetic Data STD: {synth_std}', ha='left', va='top')
+
+            text_ax.text(0.01, 0.65, f'Ground Truth Skew: {gt_skew}', ha='left', va='top')
+            text_ax.text(0.51, 0.65, f'Synthetic Data Skew: {synth_skew}', ha='left', va='top')
+
+            text_ax.text(0.01, 0.50,f'Ground Truth Kurtosis: {gt_kurtosis}', ha='left', va='top')
+            text_ax.text(0.51, 0.50, f'Synthetic Data Kurtosis: {synth_kurtosis}', ha='left', va='top')
+
+            text_ax.set_xticks([])
+            text_ax.set_yticks([])
+
+            if itr==0:
+                fft_ax.set_title("Heartrate FFT")
+                wave_ax.set_ylabel("R-R Interval (s)")
+                wave_ax.set_title("Scaled R-R Distances")
+                wave_ax.set_xlabel("Time (s)")
+
+            else:
+                wave_ax.set_xticks([])
+                fft_ax.set_xticks([])
 
     plt.suptitle(name)
-    plt.savefig(f'figures/{name}.png')
+    plt.savefig('figures/datamoments.png')
     plt.show()
 
-first_half, second_half, heartrate_min, heartrate_max = split_data(patient_names[0])
+
+patient = patient_names[0]
+first_half, second_half, heartrate_min, heartrate_max = split_data(patient)
 
 first_half_dl, first_half_test = make_GAN_test_set(first_half)
 second_half_dl, second_half_test = make_GAN_test_set(second_half)
 
 
-wgan_gp_first = train_store_gan(first_half_dl, 'first_half', just_load=True, epochs=5000)
-wgan_gp_second = train_store_gan(second_half_dl, 'second_half', just_load=True, epochs=5000)
+wgan_gp_first = train_store_gan(first_half_dl, 'first_half', patient, just_load=True, epochs=5000)
+wgan_gp_second = train_store_gan(second_half_dl, 'second_half', patient, just_load=True, epochs=5000)
 synth_first = get_synthetic_outputs(wgan_gp_first, iterations=len(first_half_test))
-synth_second = get_synthetic_outputs(wgan_gp_second, iterations=int(len(second_half_test)/2))
-final_plot([synth_first, synth_second], [first_half_test,second_half_test], data_max=heartrate_max, data_min=heartrate_min, name='Patient 06-31-24 R-R First Second Half')
+synth_second = get_synthetic_outputs(wgan_gp_second, iterations=len(second_half_test))
+final_plot([synth_first, synth_second], [first_half_test,second_half_test], data_max=heartrate_max, data_min=heartrate_min, name='Patient 06-31-24 R-R Synthetic Data Statistical Moments')
